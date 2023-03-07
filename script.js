7694f7e4f967569dcd189b6b827de61832316ab1
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-03-08 00:19:52 +0400

    update
    adding JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//////////////////////////////////
+
+const iconsBtn = document.querySelectorAll(".icon");
+const itemElement = document.querySelectorAll(".item");
+
+for (let i = 0; i < iconsBtn.length; i++) {
+  iconsBtn[i].addEventListener("click", function (e) {
+    for (let j = 0; j < itemElement.length; j++) {
+      itemElement[i].classList.toggle("open");
+    }
+  });
+}
