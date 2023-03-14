f1ddcedc2fc553376c0db6e704f0e7bdca4b32c6
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-03-15 00:33:52 +0400

    update
    adding arrow rotation

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ for (let i = 0; i < iconsBtn.length; i++) {
   iconsBtn[i].addEventListener("click", function (e) {
     for (let j = 0; j < itemElement.length; j++) {
       itemElement[i].classList.toggle("open");
+      iconsBtn[i].classList.toggle("arrow");
     }
   });
 }
